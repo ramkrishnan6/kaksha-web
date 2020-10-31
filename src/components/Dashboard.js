b0fd70022600be9d0713f2caf2914beef9b0b5b6
Ram Krishnan <ramkrishnan@live.com>
2020-11-01 02:10:30 +0530

    Add Dashboard Component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useState } from "react";
+import Header from "./Header";
+
+function Dashboard() {
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+
+    const updateName = (firstName, lastName) => {
+        setFirstName(firstName);
+        setLastName(lastName);
+    };
+
+    const requestHeader = {
+        headers: {
+            "auth-token": localStorage.getItem("auth-token"),
+        },
+    };
+
+    axios
+        .get("user/dashboard", requestHeader)
+        .then((res) =>
+            updateName(res.data.data.first_name, res.data.data.last_name)
+        )
+        .catch((err) => console.log(err));
+
+    return (
+        <>
+            <Header />
+            <h2>
+                {firstName} {lastName}'s Dashboard
+            </h2>
+        </>
+    );
+}
+
+export default Dashboard;
