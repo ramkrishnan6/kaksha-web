a441a51faafff265738fd63008e1f94dc43929be
Ram Krishnan <ramkrishnan@live.com>
2020-11-01 02:11:11 +0530

    Define private routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
     return (
-        <Container
-            className="d-flex align-items-center justify-content-center"
-            style={{ minHeight: "80vh" }}
-        >
-            <div className="w-100" style={{ maxWidth: "400px" }}>
-                <Router>
-                    <Switch>
-                        <Route path="/register" component={Register} />
-                        <Route path="/login" component={Login} />
-                    </Switch>
-                </Router>
-            </div>
-        </Container>
+        <div>
+            <Router>
+                <PrivateRoute exact path="/dashboard">
+                    <Dashboard />
+                </PrivateRoute>
+
+                <Container
+                    className="d-flex align-items-center justify-content-center"
+                    style={{ minHeight: "80vh" }}
+                >
+                    <div className="w-100" style={{ maxWidth: "400px" }}>
+                        <Switch>
+                            <Route
+                                exact
+                                path="/register"
+                                component={Register}
+                            />
+                            <Route exact path="/login" component={Login} />
+                        </Switch>
+                    </div>
+                </Container>
+            </Router>
+        </div>
     );
 }
 
