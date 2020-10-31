20dc1e63bb731620ef621decab89e39f12ff6550
Ram Krishnan <ramkrishnan@live.com>
2020-10-31 23:39:18 +0530

    Integrate Register User API

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,35 +1,106 @@
-import React from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+    const emailRef = useRef();
+    const roleRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function registerUser(e) {
+        e.preventDefault();
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError("Passwords do not match");
+        }
+        setError("");
+        setLoading(true);
+
+        axios
+            .post("user/register", {
+                first_name: firstNameRef.current.value,
+                last_name: lastNameRef.current.value,
+                email: emailRef.current.value,
+                password: passwordRef.current.value,
+                role: roleRef.current.value,
+            })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                setError(err.response.data.message);
+            });
+        setLoading(false);
+    }
+
     return (
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
-                    <Form>
+                    <h2 className="text-center mb-4">Register</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+
+                    <Form onSubmit={registerUser}>
                         <Form.Group id="first_name">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" required />
+                            <Form.Control
+                                type="text"
+                                ref={firstNameRef}
+                                required
+                            />
                         </Form.Group>
+
                         <Form.Group id="last_name">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" required />
+                            <Form.Control
+                                type="text"
+                                ref={lastNameRef}
+                                required
+                            />
                         </Form.Group>
+
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" required />
+                            <Form.Control
+                                type="email"
+                                ref={emailRef}
+                                required
+                            />
                         </Form.Group>
+
+                        <Form.Group id="role">
+                            <Form.Label>Role</Form.Label>
+                            <Form.Control type="text" ref={roleRef} required />
+                        </Form.Group>
+
                         <Form.Group id="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" required />
+                            <Form.Control
+                                type="password"
+                                ref={passwordRef}
+                                required
+                            />
                         </Form.Group>
+
                         <Form.Group id="password-confirm">
                             <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" required />
+                            <Form.Control
+                                type="password"
+                                ref={passwordConfirmRef}
+                                required
+                            />
                         </Form.Group>
-                        <Button className="w-100" type="submit">
+
+                        <Button
+                            disabled={loading}
+                            className="w-100"
+                            type="submit"
+                        >
                             Sign Up
                         </Button>
                     </Form>
