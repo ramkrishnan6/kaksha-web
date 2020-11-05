99ae70d06912621a9a52897237ec37c22a888581
Ram Krishnan <ramkrishnan@live.com>
2020-11-05 12:26:41 +0530

    Update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "./Header";
 
-function Header() {
-    return <h1>Home Page</h1>;
+function Home() {
+    return (
+        <>
+            <Header />
+            <h1>Home Page</h1>
+            <h4>
+                <a href="/login">Login</a>
+            </h4>
+            <h4>
+                <a href="/register">Register</a>
+            </h4>
+        </>
+    );
 }
 
-export default Header;
+export default Home;
