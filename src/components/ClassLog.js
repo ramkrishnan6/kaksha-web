7fc8072a2b98c5a7633d450e9f1e3b9ccbd4cb14
Ram Krishnan <ramkrishnan@live.com>
2020-11-05 21:48:22 +0530

    Fix incorrect time in report for ongoing classes

--- src/components/ClassLog.js
+++ src/components/ClassLog.js
@@ -6,7 +6,13 @@ function Report({ classLogs }) {
         <>
             <h2>Class - {classLogs.number}</h2>
             <p>Started at {new Date(classLogs.started_at).toLocaleString()}</p>
-            <p> Ended at {new Date(classLogs.ended_at).toLocaleString()}</p>
+            <p>
+                {" "}
+                Ended at{" "}
+                {classLogs.ended_at
+                    ? new Date(classLogs.ended_at).toLocaleString()
+                    : ""}
+            </p>
             <p> {classLogs.is_active ? "In progress" : "Finished"}</p>
             <ClassUserLog userLogs={classLogs["logs"]} />
         </>
